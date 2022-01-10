2b3430979b8c37da494543fea10f92c0791b1129
injunyee <ghkd306@gmail.com>
2022-01-10 11:10:37 +0900

    asdfasdfasdf

--- com.hij.s1.member/src/com/hij/test/Test3.java
+++ com.hij.s1.member/src/com/hij/test/Test3.java
@@ -1,10 +1,10 @@
 package com.hij.test;
 
-public class Test3
+class Test4
 {
-	public Test3() {}
+	public Test4() {}
 	
-	public Test3(int a)
+	public Test4(int a)
 	{
 		System.out.printf("hellow world\n");
 	}
@@ -13,3 +13,38 @@ public class Test3
 	
 	Test3 t3 = new Test3();
 }
+
+class Unit
+{
+	int a;
+	int b;
+	public void hi()
+	{
+		System.out.printf("hellow");
+	}
+}
+
+public class Test3
+{
+	public static void main(String[] args)
+	{
+		int i = 10;
+		Unit unit[] = new Unit[i];
+		for(i=0; i<10; i++)
+		{
+			unit[i] = new Unit();
+		}
+		
+		for(i=0; i<unit.length; i++)
+		{
+			System.out.print(unit[i] + "\n");
+		}
+	}
+}
+
+
+
+
+
+
+
